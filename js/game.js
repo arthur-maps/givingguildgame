b3c789dc1dfd07255ef285509fe2ca8fa51f44b5
arthur-maps <52044975+arthur-maps@users.noreply.github.com>
2026-07-10 14:21:31 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,47 +1,142 @@
 import Player from "./player.js";
 import Camera from "./camera.js";
-import World from "./world.js";
 
-const canvas=document.getElementById("game");
-const ctx=canvas.getContext("2d");
+import World from "./world/world.js";
+import Sky from "./world/sky.js";
+
+import Intro from "./engine/intro.js";
+import Particles from "./engine/particles.js";
+import Lighting from "./engine/lighting.js";
+
+
+const canvas =
+document.getElementById("game");
+
+
+const ctx =
+canvas.getContext("2d");
+
 
 function resize(){
 
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width=innerWidth;
+canvas.height=innerHeight;
 
 }
 
 resize();
 
-window.addEventListener("resize",resize);
+window.onresize=resize;
 
-const player=new Player();
 
-const camera=new Camera();
 
-const world=new World();
+const player =
+new Player();
 
-document.body.addEventListener("click",()=>{
 
-document.getElementById("music").play();
+const camera =
+new Camera();
 
-},{once:true});
 
-function animate(){
+const world =
+new World();
 
-requestAnimationFrame(animate);
+
+const sky =
+new Sky();
+
+
+const intro =
+new Intro();
+
+
+const particles =
+new Particles();
+
+
+const lighting =
+new Lighting();
+
+
+
+let time=0;
+
+
+
+function loop(){
+
+
+requestAnimationFrame(loop);
+
+
+time++;
+
+
+
+ctx.clearRect(
+0,
+0,
+canvas.width,
+canvas.height
+);
+
+
+
+sky.draw(
+ctx,
+canvas.width,
+canvas.height,
+time
+);
+
+
 
 player.update();
 
+
 camera.update(player);
 
-ctx.clearRect(0,0,canvas.width,canvas.height);
 
-world.draw(ctx,camera);
 
-player.draw(ctx,camera);
+world.draw(
+ctx,
+camera
+);
+
+
+
+particles.update();
+
+particles.draw(
+ctx,
+camera
+);
+
+
+
+lighting.update();
+
+
+lighting.draw(
+ctx,
+canvas.width,
+canvas.height
+);
+
+
+
+intro.update();
+
+
+intro.draw(
+ctx,
+canvas.width,
+canvas.height
+);
+
 
 }
 
-animate();
+
+
+loop();
